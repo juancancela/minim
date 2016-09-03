837bc8dc5b9f27e669895b5b6a77aa7fef07dfc4
Juan Cancela <cancela.juancarlos@gmail.com>
2016-09-03 11:46:55 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const imagemin = require('imagemin');
+const imageminMozjpeg = require('imagemin-mozjpeg');
+const imageminPngquant = require('imagemin-pngquant');
+
+imagemin(['images/*.{jpg,png}'], 'build/images', {
+    plugins: [
+        imageminMozjpeg(),
+        imageminPngquant({quality: '65-80'})
+    ]
+}).then(files => {
+    console.log(files);
+    //=> [{data: <Buffer 89 50 4e …>, path: 'build/images/foo.jpg'}, …]
+});
